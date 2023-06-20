12ff345adc3a05d7269c31fabc01f49048210ced
Manoel Carvalho <francisco.carvalho@sof.to>
2023-06-20 10:09:22 -0300

    Add typeORM, config env and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_DB,
+      port: Number(process.env.DATABASE_PORT),
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USER,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
